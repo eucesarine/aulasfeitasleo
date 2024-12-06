c9b0e42552243c90996778db12745fe78e94a1b3
Yun Li ' Version <166619941+eucesarine@users.noreply.github.com>
2024-12-06 11:27:51 -0300

    Add files via upload

---
+++ patinhos burrinhos/05-12/script.js
@@ -0,0 +1,18 @@
+
+const meuParagrafo = document.getElementById('texto');
+const meuBotao = document.getElementById('meuBotao');
+
+const bota2 = document.getElementById('botao2');
+
+
+meuBotao.addEventListener('click', function() {
+
+    meuParagrafo.textContent = 'oi, meu nobre!';
+})
+
+botao2.addEventListener('click', function() {
+ 
+       alteraTexto2.textContent = 'gay?👻';
+})
+
+botao.addEventListener("click", alteraTexto)
